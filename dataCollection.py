13af87891d2b66d597cf466e8d515dbe970e37ef
Munchi <148445538+Reo69@users.noreply.github.com>
2024-02-18 16:34:39 +0530

    Documentation

--- dataCollection.py
+++ dataCollection.py
@@ -7,27 +7,33 @@ import time
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1, detectionCon=0.8)
 
-# Constants
+# Constants :
 
 OFFSET = 20
+#   OFFSETS the cropped image to capture the entire hand
 IMG_SIZE = 300
-# Variables
+#   Defines the size of imgwhite
+
+# Variables :
 
 counter = 0
 folder = "Model_Data/Hello"
+#   Path to save samples
 
 while True:
     try:
         success, img = cap.read()
         hands, img = detector.findHands(img)
+        #   Finds Hands from img
         if hands:
             hand = hands[0]
             x, y, w, h = hand['bbox']
+            #   returns the Bounding Box attributes of hand
 
             imgWhite = np.ones((IMG_SIZE, IMG_SIZE, 3), np.uint8) * 255
+            #   Initialize white background to put cropped image onto
             imgCrop = img[y-OFFSET: y + h + OFFSET, x-OFFSET: x + w + OFFSET]
-
-            imgCropShape = imgCrop.shape
+            #   Crops Hand with Bone structure
 
             aspectRatio = h/w
 
@@ -38,6 +44,7 @@ while True:
                 imgResizeShape = imgResize.shape
                 wGap = math.ceil((IMG_SIZE - wCal)/2)
                 imgWhite[:, wGap: wCal + wGap] = imgResize
+                #   Pastes imgCrop onto imgWhite
 
             else:
                 k = IMG_SIZE / w
@@ -46,16 +53,21 @@ while True:
                 imgResizeShape = imgResize.shape
                 hGap = math.ceil((IMG_SIZE - hCal) / 2)
                 imgWhite[hGap: hCal + hGap, :] = imgResize
+                #   Pastes imgCrop onto imgWhite
 
             cv2.imshow("ImageCrop", imgCrop)
+            #   Display cropped hand
             cv2.imshow("ImageWhite", imgWhite)
         cv2.imshow("Image", img)
         key = cv2.waitKey(1)
 
+#   Collecting samples  :
         if key == ord("s"):
             counter += 1
             cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
+            #   Saves samples in the specified folder
             print(counter)
     except cv2.error:
+        # Prevents crash when hand is present outside the frame
         print("Cannot detect (Out of Bounds)")
 
