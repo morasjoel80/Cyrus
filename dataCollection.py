b6ac12e57cd7b63896af4717211b172dd252440d
Munchi <148445538+Reo69@users.noreply.github.com>
2024-02-18 16:19:37 +0530

    Prevent crash when out of bounds

--- dataCollection.py
+++ dataCollection.py
@@ -11,53 +11,51 @@ detector = HandDetector(maxHands=1, detectionCon=0.8)
 
 OFFSET = 20
 IMG_SIZE = 300
-SAMPLE_SIZE = 500
 # Variables
 
 counter = 0
 folder = "Model_Data/Hello"
 
 while True:
-    success, img = cap.read()
-    hands, img = detector.findHands(img)
-    if hands:
-        hand = hands[0]
-        x, y, w, h = hand['bbox']
+    try:
+        success, img = cap.read()
+        hands, img = detector.findHands(img)
+        if hands:
+            hand = hands[0]
+            x, y, w, h = hand['bbox']
 
-        imgWhite = np.ones((IMG_SIZE, IMG_SIZE, 3), np.uint8) * 255
-        imgCrop = img[y-OFFSET: y + h + OFFSET, x-OFFSET: x + w + OFFSET]
+            imgWhite = np.ones((IMG_SIZE, IMG_SIZE, 3), np.uint8) * 255
+            imgCrop = img[y-OFFSET: y + h + OFFSET, x-OFFSET: x + w + OFFSET]
 
-        imgCropShape = imgCrop.shape
+            imgCropShape = imgCrop.shape
 
-        aspectRatio = h/w
+            aspectRatio = h/w
 
-        if aspectRatio > 1:
-            k = IMG_SIZE/h
-            wCal = math.ceil(k * w)
-            imgResize = cv2.resize(imgCrop, (wCal, IMG_SIZE))
-            imgResizeShape = imgResize.shape
-            wGap = math.ceil((IMG_SIZE - wCal)/2)
-            imgWhite[:, wGap: wCal + wGap] = imgResize
+            if aspectRatio > 1:
+                k = IMG_SIZE/h
+                wCal = math.ceil(k * w)
+                imgResize = cv2.resize(imgCrop, (wCal, IMG_SIZE))
+                imgResizeShape = imgResize.shape
+                wGap = math.ceil((IMG_SIZE - wCal)/2)
+                imgWhite[:, wGap: wCal + wGap] = imgResize
 
-        else:
-            k = IMG_SIZE / w
-            hCal = math.ceil(k * h)
-            imgResize = cv2.resize(imgCrop, (IMG_SIZE, hCal))
-            imgResizeShape = imgResize.shape
-            hGap = math.ceil((IMG_SIZE - hCal) / 2)
-            imgWhite[hGap: hCal + hGap, :] = imgResize
+            else:
+                k = IMG_SIZE / w
+                hCal = math.ceil(k * h)
+                imgResize = cv2.resize(imgCrop, (IMG_SIZE, hCal))
+                imgResizeShape = imgResize.shape
+                hGap = math.ceil((IMG_SIZE - hCal) / 2)
+                imgWhite[hGap: hCal + hGap, :] = imgResize
 
-        cv2.imshow("ImageCrop", imgCrop)
-        cv2.imshow("ImageWhite", imgWhite)
-    cv2.imshow("Image", img)
-    key = cv2.waitKey(1)
+            cv2.imshow("ImageCrop", imgCrop)
+            cv2.imshow("ImageWhite", imgWhite)
+        cv2.imshow("Image", img)
+        key = cv2.waitKey(1)
 
-    if key == ord("s"):
-        counter += 1
-        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
-        print(counter)
-
-    if counter == SAMPLE_SIZE:
-        exit()
-
+        if key == ord("s"):
+            counter += 1
+            cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
+            print(counter)
+    except cv2.error:
+        print("Cannot detect (Out of Bounds)")
 
--- test.py
+++ test.py
@@ -31,66 +31,72 @@ folder.close()
 
 
 def speech(text):
-    engine = pyttsx3.init()
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[VOICE].id)
-    engine.say(text)
-    engine.runAndWait()
+    try:
+        engine = pyttsx3.init()
+        voices = engine.getProperty('voices')
+        engine.setProperty('voice', voices[VOICE].id)
+        engine.say(text)
+        engine.runAndWait()
+    except RuntimeError:
+        print("\nSpeech error (Multiple inputs)")
 
 
 def capture():
     text = ""
 
     while True:
-        success, img = cap.read()
-        imgOutput = img.copy()
-        hands, img = detector.findHands(img)
-        if hands:
-            hand = hands[0]
-            x, y, w, h = hand['bbox']
+        try:
+            success, img = cap.read()
+            imgOutput = img.copy()
+            hands, img = detector.findHands(img)
+            if hands:
+                hand = hands[0]
+                x, y, w, h = hand['bbox']
 
-            imgWhite = np.ones((IMG_SIZE, IMG_SIZE, 3), np.uint8) * 255
-            imgCrop = img[y-OFFSET: y + h + OFFSET, x-OFFSET: x + w + OFFSET]
+                imgWhite = np.ones((IMG_SIZE, IMG_SIZE, 3), np.uint8) * 255
+                imgCrop = img[y-OFFSET: y + h + OFFSET, x-OFFSET: x + w + OFFSET]
 
-            imgCropShape = imgCrop.shape
+                imgCropShape = imgCrop.shape
 
-            aspectRatio = h/w
+                aspectRatio = h/w
 
-            if aspectRatio > 1:
-                k = IMG_SIZE/h
-                wCal = math.ceil(k * w)
-                imgResize = cv2.resize(imgCrop, (wCal, IMG_SIZE))
-                imgResizeShape = imgResize.shape
-                wGap = math.ceil((IMG_SIZE - wCal)/2)
-                imgWhite[:, wGap: wCal + wGap] = imgResize
-                prediction, index = classifier.getPrediction(imgWhite)
-                print(prediction, index)
+                if aspectRatio > 1:
+                    k = IMG_SIZE/h
+                    wCal = math.ceil(k * w)
+                    imgResize = cv2.resize(imgCrop, (wCal, IMG_SIZE))
+                    imgResizeShape = imgResize.shape
+                    wGap = math.ceil((IMG_SIZE - wCal)/2)
+                    imgWhite[:, wGap: wCal + wGap] = imgResize
+                    prediction, index = classifier.getPrediction(imgWhite)
+                    print(prediction, index)
 
-            else:
-                k = IMG_SIZE / w
-                hCal = math.ceil(k * h)
-                imgResize = cv2.resize(imgCrop, (IMG_SIZE, hCal))
-                imgResizeShape = imgResize.shape
-                hGap = math.ceil((IMG_SIZE - hCal) / 2)
-                imgWhite[hGap: hCal + hGap, :] = imgResize
-                prediction, index = classifier.getPrediction(imgWhite)
+                else:
+                    k = IMG_SIZE / w
+                    hCal = math.ceil(k * h)
+                    imgResize = cv2.resize(imgCrop, (IMG_SIZE, hCal))
+                    imgResizeShape = imgResize.shape
+                    hGap = math.ceil((IMG_SIZE - hCal) / 2)
+                    imgWhite[hGap: hCal + hGap, :] = imgResize
+                    prediction, index = classifier.getPrediction(imgWhite)
 
-    # Output Box
+        # Output Box
 
-            cv2.putText(imgOutput, Labels[index], (x, y-OFFSET), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 2)
+                cv2.putText(imgOutput, Labels[index], (x, y-OFFSET), cv2.FONT_HERSHEY_COMPLEX, 2, (255, 0, 255), 2)
 
-    # Text To Speech
+        # Text To Speech
 
-            if text != Labels[index]:
-                text = Labels[index]
-                threading.Thread(
-                    target=speech, args=text, daemon=True
-                ).start()
+                if text != Labels[index]:
+                    text = Labels[index]
+                    threading.Thread(
+                        target=speech, args=text, daemon=True
+                    ).start()
 
-            # cv2.imshow("ImageCrop", imgCrop)
-            # cv2.imshow("ImageWhite", imgWhite)
-        cv2.imshow("Image", imgOutput)
-        cv2.waitKey(1)
+                # cv2.imshow("ImageCrop", imgCrop)
+                # cv2.imshow("ImageWhite", imgWhite)
+            cv2.imshow("Image", imgOutput)
+            cv2.waitKey(1)
+        except cv2.error:
+            print("\nCannot Detect (Out of Bounds)")
 
 
 if __name__ == "__main__":
